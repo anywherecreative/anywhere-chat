675b64cdf21022e2ad85742a23ff197216f7aea1
Jeff Manning <jeff@anywherecreative.ca>
2018-05-20 14:09:19 -0400

    bootstrap && session stuff

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var session = require("express-session")({
+    secret: "7uyz87f8vd18lkuiilv0",
+    resave: true,
+    saveUninitialized: true
+  }),
+var sharedsession = require("express-socket.io-session");
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
